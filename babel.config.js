2af9754b0bb7f4d86515bae11e54ccb6f1cc987c
Alyona Irisova <elaineirr.910@gmail.com>
2021-03-26 22:30:49 +0400

    feat: add webpack configuration

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+const presets = [
+  ['@babel/env', {
+    targets: {
+      edge: '17',
+      ie: '11',
+      firefox: '50',
+      chrome: '64',
+      safari: '11.1'
+    },
+    useBuiltIns: "entry"
+  }]
+];
+
+module.exports = { presets };
